7021f6a1b09d8f997af65cad5c748934d3e723ac
Versha1234 <vershakrishnani96642@gmail.com>
2022-04-28 10:00:49 +0530

    changes done

--- assignment.java
+++ assignment.java
@@ -1,7 +1,29 @@
-class emp{
-public static void main(String args[]){
-    System.out.println("hello world");
-    System.out.println("my name is versha");
-    System.out.println("abc");
+class employee {
+
+	public int add() {
+		int a=20;
+		int b=10;
+		int c=a+b;
+		System.out.println(c);
+		return c;
+	}
+	public static int sub() {
+		int a=20;
+		int b=10;
+		int c=a-b;
+		System.out.println(c);
+		return c;
+	}
+ }
+class Demo1{
+
+	public static void main(String[] args) {
+		employee obj=new employee();
+		obj.add();
+		obj.sub();
+		// TODO Auto-generated method stub
+
+	}
+
 }
-}
+
